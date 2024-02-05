4c739b93fd0269ea51e245a77aec24e95b4982a9
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-02-05 09:44:59 -0700

    Add initial project files with working HB template

---
+++ server.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const session = require('express-session');
+const handlebars = require('express-handlebars');
+const routes = require('./controllers');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+    }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on: http://localhost:${PORT}`);
+});
