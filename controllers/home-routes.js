6736b6a51fea904873f448909b76da3d66427c2a
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-02-05 15:17:37 -0700

    Add signup, login, and logout routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,4 +4,18 @@ router.get('/', (req, res) => {
     res.render('home', { layout: 'main' });
 });
 
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/');
+    });
+});
+
 module.exports = router;
