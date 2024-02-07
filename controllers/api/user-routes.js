9ab7d3b4430b4597b2b5f4bdbf60ffbc80d8fe4e
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-02-06 19:39:29 -0700

    Refactor user login and signup routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const bcrypt = require('bcrypt');
 
 // User Login ('/api/user/login')
 router.post('/login', async (req, res) => {
     try {
+        console.log('Login request body:', req.body);
         const user = await User.findOne({
             where: {
-                username: req.body.username,
+                email: req.body.email,
             },
         });
 
@@ -15,7 +17,7 @@ router.post('/login', async (req, res) => {
             return;
         }
 
-        const validPassword = user.checkPassword(req.body.password);
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
 
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect username or password, please try again' });
@@ -27,11 +29,13 @@ router.post('/login', async (req, res) => {
             req.session.username = user.username;
             req.session.loggedIn = true;
 
+            console.log('User logged in successfully');
+            console.log('Session:', req.session);
             res.redirect('/');
         });
     } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+        console.log('Login error:', err);
+        res.status(500).json({ error: 'An error occurred while logging in' });
     }
 });
 
@@ -49,10 +53,15 @@ router.post('/logout', (req, res) => {
 // User Signup ('/api/user)
 router.post('/', async (req, res) => {
     try {
+        console.log('Request body:', req.body);
+
         const newUser = await User.create({
             username: req.body.username,
+            email: req.body.email,
             password: req.body.password,
         });
+
+        console.log('New user:', newUser);
 
         req.session.save(() => {
             req.session.user_id = newUser.id;
@@ -62,6 +71,7 @@ router.post('/', async (req, res) => {
             res.redirect('/');
         });
     } catch (err) {
+        console.error('Error creating new user:', err);
         res.status(500).json(err);
     }
 });
