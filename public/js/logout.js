9617c8ccc26fe3922d91e6b04d9650625f517c76
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-02-07 01:41:57 -0700

    Update logout functionality and improve UI

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,17 @@
 const logout = async () => {
+  console.log('Sending logout request...')
   const response = await fetch('/api/user/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
+    console.log("User logged out")
     document.location.replace('/login');
+    alert("User logged out")
   } else {
-    alert(response.statusText);
+    alert('Error logging out');
   }
 };
 
-document.querySelector('#logout').addEventListener('click', logout);
+document.querySelector('.logout').addEventListener('click', logout);
--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,9 @@
 const withAuth = (req, res, next) => {
     if (!req.session.user_id) {
-        console.log('User is authenticated');
+        console.log('User is not authenticated');
         res.redirect('/login');
     } else {
-        console.log('User is not authenticated');
+        console.log('User authenticated');
         console.log('User ID:', req.session.user_id);
         console.log('Username:', req.session.username);
         next();
