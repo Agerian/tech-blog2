563dc041e06c0e0447f6543ffdadbd4493ed4f0d
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-02-06 19:42:29 -0700

    Update validation length in User model and add console logs in auth middleware

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,11 @@
 const withAuth = (req, res, next) => {
     if (!req.session.user_id) {
+        console.log('User is authenticated');
         res.redirect('/login');
     } else {
+        console.log('User is not authenticated');
+        console.log('User ID:', req.session.user_id);
+        console.log('Username:', req.session.username);
         next();
     }
 };
