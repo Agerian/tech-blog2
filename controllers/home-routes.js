e41bf1699051f10a68904214eec46c82fc0f90bb
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-02-06 19:38:47 -0700

    Update user attribute in Post include

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,7 +6,7 @@ const withAuth = require('../utils/auth');
 router.get('/', withAuth, async (req, res ) => {
     try {
         const postData = await Post.findAll({
-            include: [{ model: User, attributes: ['name'] }],
+            include: [{ model: User, attributes: ['username'] }],
         });
 
         const posts = postData.map((post) => post.get({ plain: true }));
